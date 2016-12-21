c2ca0ce12ee86a9e182dadd88c08d0afcd870362
Tomasz Gemza <t.gemza@secit.pl>
2016-12-21 16:15:52 +0100

    Added jsonLd twig filter.

---
+++ Twig/JsonLdExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SecIT\JsonLdBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Class JsonLdExtension.
+ *
+ * @author Tomasz Gemza
+ */
+class JsonLdExtension extends \Twig_Extension implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('jsonLd', [$this, 'jsonLdFilter'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'secit_json_ld_extension';
+    }
+
+    /**
+     * Generate JSON-LD.
+     *
+     * @param object $object
+     *
+     * @return string
+     */
+    public function jsonLdFilter($object)
+    {
+        return $this->container->get('secit.json_ld')->generate($object);
+    }
+}
